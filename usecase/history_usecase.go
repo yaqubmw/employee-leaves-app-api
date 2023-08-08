fa897dfb8235a6509e4f1b90bf1531fd0e8e5b75
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2023-08-08 16:07:42 +0700

    wip: history usecase

---
+++ usecase/history_usecase.go
@@ -0,0 +1,32 @@
+package usecase
+
+import (
+	"employeeleave/model"
+	"employeeleave/repository"
+)
+
+type HistoryUseCase interface {
+	RegisterNewHistory(payload model.HistoryLeave) error
+	FindAllHistory() ([]model.HistoryLeave, error)
+	FindByHistoryId(id string) (model.HistoryLeave, error)
+}
+
+type historyUseCase struct {
+	repo repository.HistoryRepository
+}
+
+func (h *historyUseCase) FindAllHistory() ([]model.HistoryLeave, error) {
+	return h.repo.List()
+}
+
+func (h *historyUseCase) FindByHistoryId(id string) (model.HistoryLeave, error) {
+	return h.repo.GetHistory(id)
+}
+
+func (h *historyUseCase) RegisterNewHistory(payload model.HistoryLeave) error {
+	return h.repo.Create(payload)
+}
+
+func NewHistoryUseCase(repo repository.RoleRepository) RoleUseCase {
+	return &roleUseCase{repo: repo}
+}
